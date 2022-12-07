e35febf6239ffa5376fbbd1343c26be721a6ed6c
LieStage <118051769+LieStage@users.noreply.github.com>
2022-12-07 23:32:10 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -21,7 +21,7 @@ class Var(object):
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "945284066").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = str(getenv('APP_NAME','unfortunate-malissa-aakash2021-cap.koyeb.app'))
+    APP_NAME = str(getenv('APP_NAME','grim-cordie-aakash2021-cap.koyeb.app'))
     OWNER_USERNAME = str(getenv('OWNER_USERNAME',"FLIGHER"))
    # if 'DYNO' in environ:
        # ON_HEROKU = True
